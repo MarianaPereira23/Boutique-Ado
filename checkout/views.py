6f2a1becf06134d0d6a96cc422f6161f1fa7156d
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-08-10 14:36:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HEbwcGqyBIosGXzP0c1ynXkyGX4gE4rJi21IVrinaQkMwq6pPFw0AQDabk19a4SHAIoroOguD6lDFmINmUybGfd00N6L8fA9I',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
